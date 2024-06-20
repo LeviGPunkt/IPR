43765ec9dc18666c6f6f035faae02d64df46bd17
LeviGrausam <85581611+LeviGPunkt@users.noreply.github.com>
2024-06-20 15:05:39 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import "@/styles/globals.css";
+import '../styles/globals.css'
+import 'react-toastify/dist/ReactToastify.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Layout from '../komponenten/Layout'
+import '../styles/custom.scss'
+import store from '../redux/store'
+import { Provider } from 'react-redux'
+import { ToastContainer } from "react-toastify"
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <Provider store={store}>
+      <ToastContainer />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
 }
+
+export default MyApp
